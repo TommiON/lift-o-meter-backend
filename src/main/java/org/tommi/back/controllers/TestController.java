31059a6f3e71fbb7602837e5d10d6a860bb8ddcd
Tommi Niittymies <tommi.niittymies@gmail.com>
2022-02-23 21:53:59 +0200

    Modifications to TestController endpoint

--- src/main/java/org/tommi/back/controllers/TestController.java
+++ src/main/java/org/tommi/back/controllers/TestController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
@@ -14,6 +16,9 @@ public class TestController {
     public String allAccess() {
         return "Up and running...";
     }
+
+    @GetMapping("/time")
+    public String time() { return LocalDateTime.now().toString(); }
 
     @GetMapping("/user")
     @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
