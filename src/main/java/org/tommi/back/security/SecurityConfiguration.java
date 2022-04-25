c99b39af1ddbe44f28a78599c8f9726050f59219
TommiON <tommi.niittymies@gmail.com>
2022-04-25 20:15:44 +0300

    Troubleshooting CORS

--- src/main/java/org/tommi/back/security/SecurityConfiguration.java
+++ src/main/java/org/tommi/back/security/SecurityConfiguration.java
@@ -17,6 +17,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableWebSecurity
 //@EnableGlobalMethodSecurity(
@@ -79,8 +81,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
+        config.setAllowedOrigins(Arrays.asList("http://lift-o-meter-front.herokuapp.com", "http://localhost:3000", "http://lift-o-meter-front.herokuapp.com"));
+        config.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+        //config.addAllowedOrigin("*");
+        //config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
